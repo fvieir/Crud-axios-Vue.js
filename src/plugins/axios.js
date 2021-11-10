163f22ae654a9eabf07fd358e2b487dffa0cb3b5
fvieira2 <fabrizio003@hotmail.com>
2021-11-09 22:16:45 -0300

    intercpetores vue

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -5,6 +5,12 @@ axios.defaults.baseURL = 'https://curso-vue-1b5f6-default-rtdb.firebaseio.com/'
 
 Vue.use({
     install(Vue) {
-         Vue.prototype.$http = axios
-   }
+        Vue.prototype.$http = axios
+        Vue.prototype.$http.interceptors.request.use(config => {
+            // eslint-disable-next-line no-console
+            console.log(config)
+            return config
+        })
+    }
 })
+
